f8cc4367aa08bb27a947b62551fda48a12eb52f8
RobschCocovic <robertcovaci30@gmail.com>
2023-06-13 20:21:01 +0200

    updates

--- index.php
+++ index.php
@@ -13,22 +13,15 @@
 
 
 
-  <!--• Kunden werden uber eine Kundennummer identifiziert ¨
+  <!--• 
+  Kunden werden uber eine Kundennummer identifiziert ¨
 • Kunden haben einen Vorname und einen Nachnamen
-• NFC-User weden uber Loxone zur Verf ¨ ugung gestellt, gehen Sie daher vorerst von einer fertigen Liste an ¨
-Usern aus
 • Es soll festgehalten werden, ab wann einem Kunden ein NFC-User zugeordnet wurde.
 • Es soll festgehalten werden, wann diese Zuordnung wieder endet (Ablaufdatum).
-1
-INFORMATION SYSTEMS Third Class
 • Aus Archivgrunden sollen Kunden nur archiviert, aber nicht gel ¨ ¨oscht werden, daher sehen sie ain aktiv/inaktivFeld vor.
 • Als Datenbackend k¨onnen Sie wahlweise CSV oder JSON Dateien, MongoDB als dokumentenorientierte Datenbank oder MariaDB als SQL-Datenbank w¨ahlen; strukturieren Sie ihr Programm so, dass dies
 m¨oglichst einfach auf ein anderes Backend umgestellt werden kann.
 -->
-
-  <!--Button für das Exportieren der Values-->
-  <!-- <button class="button" role="button" onclick="exportCSV()"><span class="text">Export to CSV </span></button>-->
-
 
 
 
@@ -65,10 +58,18 @@ m¨oglichst einfach auf ein anderes Backend umgestellt werden kann.
 
     <div id="divDate">
       <form id="createDate" method="post">
-        <label for="dateInput">Datum:</label>
+        <label for="dateInput">Datum von:</label>
         <input type="date" id="dateInput" name="date" required>
       </form>
     </div>
+
+    <div id="divDate2">
+      <form id="createDate" method="post">
+        <label for="dateInput">Datum bis:</label>
+        <input type="date" id="dateInput" name="date" required>
+      </form>
+    </div>
+
   </div>
 
   <div class="container2">
@@ -77,6 +78,9 @@ m¨oglichst einfach auf ein anderes Backend umgestellt werden kann.
         <input type="submit" name="insertMember" value="Insert Member">
       </form>
     </div>
+
+    <!---
+
 
     <div id="divDeleteMember">
       <form method="post">
@@ -95,6 +99,8 @@ m¨oglichst einfach auf ein anderes Backend umgestellt werden kann.
         <input type="submit" name="deleteAll" value="delete all">
       </form>
     </div>
+
+-->
 
     <div id="divShowList">
       <form method="post">
